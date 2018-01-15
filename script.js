f6fadb49a962afc7aa820becb0654bcc680f8532
Will <willscott@Wills-MacBook-Pro.local>
2018-01-16 11:38:16 +1300

    completion

--- script.js
+++ script.js
@@ -3,7 +3,23 @@ $(document).ready(function () {
         window.location = "file:///Users/willscott/Desktop/GitHub%20Repositories/wikipediaViewer/index.html";
     })
     $("#search").on("click", function () {
-        $("#replace").html('<div id="search-replaced" class="front-page-buttons"><input type="text" placeholder="Search" required id="search-bar" class="searchers"><button type="submit" id="search-button" class="searchers">Submit</button></div>');
+        $("#replace").html('<div id="search-replaced" class="front-page-buttons"><input type="search" placeholder="Search" required id="search-bar" class="searchers"><button type="submit" id="search-button" class="searchers">Submit</button></div>');
+        $("#search-button").on("click", function () {
+            var result = $("#search-bar").val();
+            var url = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=" + result;
+            $.getJSON(url, function (json) {
+                console.log(json);
+
+                function getSearchResults() {
+                    var code = "";
+                    for (i = 0; i < json[1].length; i++) {
+                        code += '<div id="search-result-' + i + '" class="front-page-buttons"><a href=' + json[3][i] + ">" + json[1][i] + "</a><p>" + json[2][i] + "</p></div>";
+                    };
+                    return code;
+                };
+                $("#search-results").html(getSearchResults());
+            });
+        });
     });
     $("#random").on("click", function () {
         window.location = "https://en.wikipedia.org/wiki/Special:Random";
