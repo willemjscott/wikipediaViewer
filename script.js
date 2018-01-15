34986853b9911c37230d6b4d4bdca45ef9aa58cf
Will <willscott@Wills-MacBook-Pro.local>
2018-01-15 16:33:28 +1300

    set uo javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("#search").on("click", function () {
+        console.log("test");
+    });
+    $("#random").on("click", function () {
+        console.log("test");
+    });
+});
