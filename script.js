3c9627e55d216e4da70a4566051dc64ee6107518
Will <willscott@Wills-MacBook-Pro.local>
2018-01-15 17:13:53 +1300

    end of day

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
+    $("#title").on("click", function () {
+        window.location = "file:///Users/willscott/Desktop/GitHub%20Repositories/wikipediaViewer/index.html";
+    })
     $("#search").on("click", function () {
-        console.log("test");
+        $("#replace").html('<div id="search-replaced" class="front-page-buttons"><input type="text" placeholder="Search" required id="search-bar" class="searchers"><button type="submit" id="search-button" class="searchers">Submit</button></div>');
     });
     $("#random").on("click", function () {
-        console.log("test");
+        window.location = "https://en.wikipedia.org/wiki/Special:Random";
     });
 });
